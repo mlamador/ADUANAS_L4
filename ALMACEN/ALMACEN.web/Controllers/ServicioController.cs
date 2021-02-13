c03d41ae5d579dc5a4675b7cc17a474870fa21b5
mlamador <myler.leny@gmail.com>
2021-02-12 22:08:34 -0600

    lista servicios

---
+++ ALMACEN/ALMACEN.web/Controllers/ServicioController.cs
@@ -0,0 +1,41 @@
+﻿using ALMACEN.web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ALMACEN.web.Controllers
+{
+    public class ServicioController : Controller
+    {
+        // GET: Servicio
+        public ActionResult Index()
+        {
+            var servicio1 = new ServicioModel();
+            servicio1.Id = 1;
+            servicio1.Descripcion = "Almacenamiento Público";
+
+            var servicio2 = new ServicioModel();
+            servicio2.Id = 2;
+            servicio2.Descripcion = "Almacenamiento Temporal";
+
+            var servicio3 = new ServicioModel();
+            servicio3.Id = 3;
+            servicio3.Descripcion = "Almacenamiento Transito";
+
+            var servicio4 = new ServicioModel();
+            servicio4.Id = 4;
+            servicio4.Descripcion = "Carga y Descarga de Mercaderia";
+
+            var listaServicios = new List<ServicioModel>();
+            listaServicios.Add(servicio1);
+            listaServicios.Add(servicio2);
+            listaServicios.Add(servicio3);
+            listaServicios.Add(servicio4);
+
+
+            return View(listaServicios);
+        }
+    }
+}
---
+++ ALMACEN/ALMACEN.web/Models/ServicioModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ALMACEN.web.Models
+{
+    public class ServicioModel
+    {
+        public int Id { get; set; }
+        public string Descripcion { get; set; }
+
+    }
+}
