d81ad8916c88b6509034b8bfcd742a1cbf3343cd
mlamador <myler.leny@gmail.com>
2021-02-12 23:44:00 -0600

    update 1

--- ALMACEN/Almacen.BL/Servicio.cs
+++ ALMACEN/Almacen.BL/Servicio.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace ALMACEN.web.Models
+namespace Almacen.BL
 {
-    public class ServicioModel
+    public class Servicio
     {
         public int Id { get; set; }
         public string Descripcion { get; set; }
--- ALMACEN/Almacen.BL/ServiciosBL.cs
+++ ALMACEN/Almacen.BL/ServiciosBL.cs
@@ -1,41 +1,39 @@
-﻿using ALMACEN.web.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace ALMACEN.web.Controllers
+namespace Almacen.BL
 {
-    public class ServicioController : Controller
+    public class ServiciosBL
     {
-        // GET: Servicio
-        public ActionResult Index()
+        public List<Servicio> ObtenerServicios()
         {
-            var servicio1 = new ServicioModel();
+            var servicio1 = new Servicio();
             servicio1.Id = 1;
             servicio1.Descripcion = "Almacenamiento General";
 
-            var servicio2 = new ServicioModel();
+            var servicio2 = new Servicio();
             servicio2.Id = 2;
             servicio2.Descripcion = "Almacenamiento Temporal";
 
-            var servicio3 = new ServicioModel();
+            var servicio3 = new Servicio();
             servicio3.Id = 3;
             servicio3.Descripcion = "Almacenamiento Transito";
 
-            var servicio4 = new ServicioModel();
+            var servicio4 = new Servicio();
             servicio4.Id = 4;
             servicio4.Descripcion = "Carga y Descarga de Mercaderia";
 
-            var listaServicios = new List<ServicioModel>();
+            var listaServicios = new List<Servicio>();
             listaServicios.Add(servicio1);
             listaServicios.Add(servicio2);
             listaServicios.Add(servicio3);
             listaServicios.Add(servicio4);
 
-
-            return View(listaServicios);
+            return listaServicios;
         }
+
     }
 }
