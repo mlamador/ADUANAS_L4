7a2198284c87f90730cbf3f3514c2db4d9333626
mlamador <myler.leny@gmail.com>
2021-02-20 17:20:07 -0600

    update editar opciones

---
+++ ALMACEN/ALMACEN.WebAdmin/Controllers/ServiciosController.cs
@@ -0,0 +1,88 @@
+﻿using Almacen.BL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ALMACEN.WebAdmin.Controllers
+{
+    public class ServiciosController : Controller
+    {
+        ServiciosBL _serviciosBL;
+
+        public ServiciosController()
+        {
+            _serviciosBL = new ServiciosBL();
+        }
+
+        // GET: Servicios
+        public ActionResult Index()
+        {
+            var listadeServicios = _serviciosBL.ObtenerServicios();
+
+            return View(listadeServicios);
+        }
+
+
+//Segunda Entrega
+//-------------------------------------------------------------------------
+
+        public ActionResult Crear()
+        {
+            var nuevoServicio = new ServiciosBL();
+
+            return View(nuevoServicio);
+        }
+
+        [HttpPost]
+
+        public ActionResult Crear(Servicio servicio)
+        {
+            _serviciosBL.GuardarServicio(servicio);
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Editar(int id)
+        {
+           var servicio = _serviciosBL.ObtenerServicio(id);
+
+            return View(servicio);
+        }
+
+        [HttpPost]
+
+        public ActionResult Editar(Servicio servicio)
+        {
+            _serviciosBL.GuardarServicio(servicio);
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Detalle(int id)
+        {
+            var servicio = _serviciosBL.ObtenerServicio(id);
+
+            return View(servicio);
+        }
+
+        public ActionResult Eliminar(int id)
+        {
+            var servicio = _serviciosBL.ObtenerServicio(id);
+
+            return View(servicio);
+        }
+
+        [HttpPost]
+
+        public ActionResult Eliminar(Servicio servicio)
+        {
+
+            _serviciosBL.EliminarServicio(servicio);
+
+            return RedirectToAction("Index");
+        }
+
+    }
+}
--- ALMACEN/Almacen.BL/ServiciosBL.cs
+++ ALMACEN/Almacen.BL/ServiciosBL.cs
@@ -24,5 +24,42 @@ namespace Almacen.BL
             return ListadeServicios;
         }
 
+//Segunda Entrega
+//------------------------------------------------------------------------------
+
+        public void GuardarServicio(Servicio servicio)
+        {
+
+            if(servicio.Id == 0)
+            {
+                _contexto.Servicios.Add(servicio);
+            }
+            else
+            {
+                var servicioExistente = _contexto.Servicios.Find(servicio.Id);
+                servicioExistente.Descripcion = servicio.Descripcion;
+                servicioExistente.Precio = servicio.Precio;
+            }
+            
+            _contexto.SaveChanges();
+        }
+
+        public Servicio ObtenerServicio(int id)
+        {
+            var servicio = _contexto.Servicios.Find(id);
+
+            return servicio;
+        }
+
+        public void EliminarServicio(int Id)
+        {
+            var servicio = _contexto.Servicios.Find(Id);
+
+            _contexto.Servicios.Remove(servicio);
+
+            _contexto.SaveChanges();
+            
+        }
+
     }
 }
